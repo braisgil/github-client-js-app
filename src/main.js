2d9e4e5e32a1080b1f723658808e21a8e1a4c225
Brais Gil <bvgil.developer@gmail.com>
2018-11-17 22:09:26 +0000

    Final code improvements

--- src/main.js
+++ src/main.js
@@ -16,18 +16,14 @@ if (dataStorage.checkUser()) {
 const searchForm = document.getElementById('searchForm');
 searchForm.addEventListener('submit', function(e) {
     const userInput = document.getElementById('userInput').value;
+    let sessionStorageUserData = JSON.parse(sessionStorage.getItem('userData'));
     if (userInput !== '') {
         github.fetchGithubUser(userInput).then(
             function(data) {
                 if (data === undefined) {
                     ui.showMessage('User Not Found', 'alert alert-danger mt-2 col-md-12');
                 } else {
-                    if (JSON.parse(sessionStorage.getItem('userData') === null)) {
-                        ui.showMessage('User Found', 'alert alert-success mt-2 col-md-12');
-                        ui.showProfile(data.userDataJSON);
-                        ui.showRepos(data.userReposJSON);
-                        dataStorage.addUser(data.userDataJSON, data.userReposJSON);
-                    } else if(data.userDataJSON.id !== JSON.parse(sessionStorage.getItem('userData')).id) {
+                    if (sessionStorageUserData === null || data.userDataJSON.id !== sessionStorageUserData.id) {
                         ui.showMessage('User Found', 'alert alert-success mt-2 col-md-12');
                         ui.showProfile(data.userDataJSON);
                         ui.showRepos(data.userReposJSON);
